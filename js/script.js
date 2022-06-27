13eaea26b5bb850bf2b7c4a24e52bac7c46d2e90
glcarranzani <glcarranzani@gmail.com>
2022-06-27 15:57:14 -0300

    desafio1

---
+++ js/script.js
@@ -0,0 +1,52 @@
+class Usuario {
+    constructor(nombre, apellido, libros, mascota){
+        this.name = nombre;
+        this.apellido = apellido;        
+        this.libros = libros;
+        this.mascota = mascota;
+    }
+
+    getFullName(){
+        console.log(`Hola ${this.name} ${this.apellido}`)
+    }
+    
+    addMascota(mascota){
+        this.mascota.push(mascota)
+        console.log((`Agregada la mascota ${mascota} a la lista de ${this.name} ${this.apellido}`))
+        
+    } 
+    
+    countMascotas(){
+        let CantidadMascotas = this.mascota.length;
+        console.log( `La cantidad de mascotas que tiene ${this.name} es: ` + CantidadMascotas)
+    }
+
+    addBook(titulo, autor){
+        return this.libros.push({titulo, autor})        
+    }
+
+    getBookNames(){
+        let librosExistentes;
+        for (let i=0; i < this.libros.length; i++){
+            librosExistentes = this.libros[i].titulo;
+            console.log(`Los libros de ${this.name}: ${[ i + 1 ]}. `  + librosExistentes);
+        }
+    }
+}
+
+let usuario1 = new Usuario('Carlos', 'Lopez', [{titulo:'El senior de los anillos', autor: 'Tolkien'}], ['roco']);
+
+console.log('-----------------')
+
+usuario1.getFullName()
+usuario1.addMascota('Pepe')
+usuario1.countMascotas()
+console.log('-----------------')
+usuario1.addBook('El Despertar de Los Dragones', 'Morgan Rice')
+usuario1.addMascota('Mora')
+usuario1.countMascotas()
+console.log('-----------------')
+usuario1.getBookNames()
+console.log('-----------------')
+console.log(usuario1.libros)
+console.log(usuario1.mascota)
