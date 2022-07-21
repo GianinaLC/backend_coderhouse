0bdb9eb180416d29b122565a20a32ffb083e4572
glcarranzani <glcarranzani@gmail.com>
2022-07-21 18:59:44 -0300

    Desafio4

---
+++ desafio-4/routes/productos.js
@@ -0,0 +1,10 @@
+/* const { Router } = require('express')
+
+const routerProductos = new Router();
+
+routerProductos.get('/productos', (req, res) => {
+    res.json( { mensaje: 'productos router' })
+})
+
+
+module.exports = routerProductos  */
